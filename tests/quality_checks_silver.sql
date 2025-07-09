815e2746a7c05aeb23c3cc45ec6f546524d64472
LogicDavid <fayodave777@gmail.com>
2025-07-09 04:52:58 +0100

    Add files via upload

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -13,6 +13,7 @@ Script Purpose:
 
 Usage Notes:
     - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
 
